622f68ac6b1965523b7e281edca65d9534ac107c
Deep parekh <parekhd03@gmail.com>
2025-09-19 17:11:07 +0530

    Project Structure with frontend and backend initialized

---
+++ backend/digital-insurance-management-system/src/test/java/tech/zeta/mavericks/digital_insurance_management_system/DigitalInsuranceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zeta.mavericks.digital_insurance_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalInsuranceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
