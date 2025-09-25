8117a6e7d820fe1055a1f553dbb41bbb1bf1c02b
Deep Parekh <parekhd03@gmail.com>
2025-09-26 00:25:47 +0530

    Fixed backend issues of cors and also integrated login with backend and created user policy store and implemented mypolices tabular view

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router';
+import router from './router'
+import store from './store'   // import the root store
 import './assets/main.css'
-import claimsStore from './store/ClaimsManagementStore/index.js' 
 
-const app = createApp(App);
-app.use(router);
-app.use(claimsStore);
-app.mount("#app");
+const app = createApp(App)
+app.use(router)
+app.use(store)              // <-- use the Vuex store here
+app.mount('#app')
